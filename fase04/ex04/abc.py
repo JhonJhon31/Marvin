568dd7b98ba775cd2bca3b66f34c02346882ab3a
JhonJhon31 <52722891+JhonJhon31@users.noreply.github.com>
2019-07-17 21:24:04 -0300

    ABCabc

---
+++ fase04/ex04/abc.py
@@ -0,0 +1,5 @@
+def abc(letra):
+    if letra[0:1] == "a" or letra[0:1] == "b" or letra[0:1] == "c" or letra[0:1] == "A" or letra[0:1] == "B" or letra[0:1] == "C":
+        return "true"
+    else:
+        return "false"
